a14e465c25c4421e4b6f9e77a06ff21d614fff06
B-raw <michaelbarry1989@hotmail.com>
2017-07-30 15:18:27 +0100

    add test for speech disturbance

--- app/abcd2score.js
+++ app/abcd2score.js
@@ -1,12 +1,20 @@
-function abcd2risk(age) {
-  return ageScore(age);
+function abcd2risk(age, systolic, diastolic) {
+  return ageScore(age) + bpScore(systolic, diastolic);
 }
 
 function ageScore(age) {
   if (age >= 60){
     return 1;
   } else {
-    return 0
+    return 0;
+  }
+}
+
+function bpScore(systolic, diastolic) {
+  if (systolic >= 140 || diastolic >= 90) {
+    return 1;
+  } else {
+    return 0;
   }
 }
 
--- test/score.test.js
+++ test/score.test.js
@@ -7,12 +7,16 @@ describe('abcd2riskcalc', function() {
     assert.equal(abcd2risk(25, 100, 50, "None", 5, false), 0);
   });
 
-  it('should calculate abcd2 for age over 60', function() {
+  it('should calculate 1 for abcd2 for age over 60', function() {
     assert.equal(abcd2risk(70, 100, 50, "None", 5, false), 1);
   });
 
-  it('should calculate abcd2 for hypertensive patient age over 60', function() {
+  it('should calculate 2 for  abcd2 for hypertensive patient age over 60', function() {
     assert.equal(abcd2risk(70, 150, 100, "None", 5, false), 2);
+  });
+
+  it('should calculate 3 for abcd2 for hypertensive patient age over 60 with speech disturbance', function() {
+    assert.equal(abcd2risk(70, 150, 100, "None", 5, false), 3);
   });
 
   // We can have more its here

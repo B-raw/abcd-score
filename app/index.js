87b6ae36e736ce6e5121d8569247c983517a2f62
B-raw <michaelbarry1989@hotmail.com>
2017-07-30 17:26:05 +0100

    all working

--- app/index.js
+++ app/index.js
@@ -7,9 +7,10 @@ $( document ).ready(function() {
     $.each($('#abcd-form').serializeArray(), function(i, field) {
       values[field.name] = field.value;
     });
-    console.log( "You clicked submit!" );
-    console.log(values)
-    debugger
-    console.log(abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), parseInt(values.duration, 10), values.clinicalFeatures, values.diabetes))
+
+    var patientScore = abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), values.clinicalFeatures, parseInt(values.duration, 10), values.diabetes)
+    console.log(patientScore)
+
+    $("#patientScore").text(patientScore)
 });
 })

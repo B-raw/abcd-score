89fca35e4fed1a0141439d23496f6e1573a759db
Matt Stammers <matthewstammers@googlemail.com>
2017-07-31 20:27:49 +0100

    Autopopulate

--- app/index.js
+++ app/index.js
@@ -12,5 +12,21 @@ $( document ).ready(function() {
     console.log(patientScore)
 
     $("#patientScore").text(patientScore)
+
 });
+
+    $("input").change(function() {
+      console.log("changed")
+   //  $("#abcd-form").submit()
+      $.each($('#abcd-form').serializeArray(), function(i, field) {
+        values[field.name] = field.value;
+      });
+
+      var patientScore = abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), values.clinicalFeatures, parseInt(values.duration, 10), values.diabetes)
+      console.log(patientScore)
+
+      $("#patientScore").text(patientScore)
+
+});
+
 })

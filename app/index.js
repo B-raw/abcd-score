cf618a7bd6e353500f2bc8333e3977ed3ad5e7a5
B-raw <michaelbarry1989@hotmail.com>
2017-07-31 21:06:23 +0100

    add risk boxes

--- app/index.js
+++ app/index.js
@@ -8,9 +8,26 @@ $( document ).ready(function() {
     });
 
     var patientScore = abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), values.clinicalFeatures, parseInt(values.duration, 10), values.diabetes)
-    console.log(patientScore)
 
     $("#patientScore").text(patientScore)
+
+    changeRiskBoxes(patientScore)
   });
 
 })
+
+function changeRiskBoxes(patientScore) {
+  if ( patientScore >= 6 ) {
+    $("#lowRisk").css('display', 'none')
+    $("#mediumRisk").css('display', 'none')
+    $("#highRisk").css('display', 'inline-block')
+  } else if (patientScore <= 3) {
+    $("#lowRisk").css('display', 'inline-block')
+    $("#mediumRisk").css('display', 'none')
+    $("#highRisk").css('display', 'none')
+  } else {
+    $("#lowRisk").css('display', 'none')
+    $("#mediumRisk").css('display', 'inline-block')
+    $("#highRisk").css('display', 'none')
+  }
+}

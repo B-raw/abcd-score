c83ef8060da04bd97106bd7edec1d4c41142dcf4
B-raw <michaelbarry1989@hotmail.com>
2017-07-31 20:34:02 +0100

    removed submit button

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
 var values = {};
 
 $( document ).ready(function() {
-  $( "#submit" ).click(function(event) {
-    event.preventDefault();
 
+  $("input").change(function() {
     $.each($('#abcd-form').serializeArray(), function(i, field) {
       values[field.name] = field.value;
     });
@@ -12,21 +11,6 @@ $( document ).ready(function() {
     console.log(patientScore)
 
     $("#patientScore").text(patientScore)
-
-});
-
-    $("input").change(function() {
-      console.log("changed")
-   //  $("#abcd-form").submit()
-      $.each($('#abcd-form').serializeArray(), function(i, field) {
-        values[field.name] = field.value;
-      });
-
-      var patientScore = abcd2risk(parseInt(values.age,10), parseInt(values.systolicBP, 10), parseInt(values.diastolicBP, 10), values.clinicalFeatures, parseInt(values.duration, 10), values.diabetes)
-      console.log(patientScore)
-
-      $("#patientScore").text(patientScore)
-
-});
+  });
 
 })
